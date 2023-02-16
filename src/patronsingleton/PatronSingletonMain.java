de6398fa5797ebc7555490f7bbc107694b9e4b6c
juanpagu2101 <guzmanpjpablo@gmail.com>
2023-02-16 08:25:35 -0500

    Patron Singleton

---
+++ src/patronsingleton/PatronSingletonMain.java
@@ -0,0 +1,21 @@
+package patronsingleton;
+
+public class PatronSingletonMain {
+    public static void main(String[] args) {
+        //Configuración de Propiedades para el Modulo No->1
+        ParametrizacionSingleton singletonA = ParametrizacionSingleton.getInstance();
+        //Configuración de Propiedades para el Modulo No->1
+        ParametrizacionSingleton singletonB = ParametrizacionSingleton.getInstance();
+        System.out.println(singletonA);
+        System.out.println(singletonB);
+        System.out.println("Igual referencia ==> " + (singletonA == singletonB));
+        singletonA.setNombreAplicacion("Patron Creacional Singleton");
+        singletonB.setNumeroVersion("2.0x");
+        System.out.println("SingletonA ==> " + singletonA);
+        System.out.println("SingletonB ==> " + singletonB);
+        singletonA = null;
+        singletonB = null;
+        singletonA = ParametrizacionSingleton.getInstance();
+        System.out.println("singletonA ==> " + singletonA);
+    }
+}
